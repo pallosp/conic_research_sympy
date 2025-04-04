ffe12c8bd72d260f311306de9736e33ad2193810
Péter Pallos <pallosp@gmail.com>
2025-04-04 12:52:50 +0200

    Test translating circles

--- tests/transform_test.py
+++ tests/transform_test.py
@@ -2,6 +2,7 @@ from sympy import Matrix
 from sympy import symbols
 from sympy import simplify
 
+from lib.circle import Circle
 from lib.central_conic import ConicCenter
 from lib.transform import TransformConic
 from lib.transform import Translate
@@ -12,6 +13,16 @@ conic = Matrix([[a, b, d], [b, c, e], [d, e, f]])
 
 class TestTranslate:
     def test_translate_circle(self):
+        x, y, r = symbols("x,y,r")
+        dx, dy = symbols("dx,dy")
+        transformation = Translate(dx, dy)
+        circle = Circle(x, y, r)
+        new_circle = TransformConic(circle, transformation)
+        new_center_x, new_center_y = ConicCenter(new_circle)
+        assert new_center_x == x + dx
+        assert new_center_y == y + dy
+
+    def test_translate_conic(self):
         center_x, center_y = ConicCenter(conic)
         dx, dy = symbols("dx,dy")
         transformation = Translate(dx, dy)
