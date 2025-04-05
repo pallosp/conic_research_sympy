a70272ff212e9d5f270ce9961232cb50558d91d6
Péter Pallos <pallosp@gmail.com>
2025-04-05 10:54:52 +0200

    Director circle matrix in adjugate form

--- director_circle.py
+++ director_circle.py
@@ -20,3 +20,14 @@ print("\nDirector circle radius:")
 pprint(radius)
 print("\nDirector circle matrix:")
 pprint(director_circle)
+
+print("\nDirector circle matrix in adjugate form:")
+a_adj, b_adj, d_adj, _, c_adj, e_adj, _, _, f_adj = conic.adjugate()
+pprint(
+    director_circle.subs(
+        zip(
+            [a_adj, b_adj, c_adj, d_adj, e_adj, f_adj],
+            symbols("a_adj,b_adj,c_adj,d_adj,e_adj,f_adj"),
+        ),
+    )
+)
