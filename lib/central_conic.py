4bb497f6e2a31f9fface2b4f50ef67b9c80f8c0d
Péter Pallos <pallosp@gmail.com>
2025-04-05 11:52:08 +0200

    Make sure that SemiMajorAxis always returns the longer axis

--- lib/central_conic.py
+++ lib/central_conic.py
@@ -1,4 +1,4 @@
-from sympy import Matrix, sqrt
+from sympy import Matrix, Piecewise, sqrt
 
 from lib.matrix import MaxEigenvalue, MinEigenvalue
 
@@ -9,13 +9,20 @@ def ConicCenter(conic: Matrix):
     return (x / z, y / z)
 
 
-def SemiMajorAxis(conic: Matrix):
+def AxisLengths(conic: Matrix):
     """Source: ChatGPT"""
     submatrix = conic[:2, :2]
-    return sqrt(-conic.det() / (MinEigenvalue(submatrix) * submatrix.det()))
+    return (
+        sqrt(-conic.det() / (MinEigenvalue(submatrix) * submatrix.det())),
+        sqrt(-conic.det() / (MaxEigenvalue(submatrix) * submatrix.det())),
+    )
+
+
+def SemiMajorAxis(conic: Matrix):
+    axes = AxisLengths(conic)
+    return Piecewise((axes[1], conic.det() > 0), (axes[0], True))
 
 
 def SemiMinorAxis(conic: Matrix):
-    """Source: ChatGPT"""
-    submatrix = conic[:2, :2]
-    return sqrt(-conic.det() / (MaxEigenvalue(submatrix) * submatrix.det()))
+    axes = AxisLengths(conic)
+    return Piecewise((axes[0], conic.det() > 0), (axes[1], True))
--- tests/central_conic_test.py
+++ tests/central_conic_test.py
@@ -1,7 +1,8 @@
 from sympy import Matrix, symbols
 
 from lib.central_conic import ConicCenter, SemiMajorAxis, SemiMinorAxis
-from lib.circle import Circle
+from lib.circle import Circle, UnitCircle
+from lib.transform import ScaleXY, TransformConic
 
 
 class TestConicCenter:
@@ -20,3 +21,10 @@ class TestAxes:
         circle = Circle(x, y, r)
         assert r == SemiMajorAxis(circle)
         assert r == SemiMinorAxis(circle)
+
+    def test_ellipse_axes(self):
+        ellipse = TransformConic(UnitCircle(), ScaleXY(2, 3))
+        assert SemiMajorAxis(ellipse) == 3
+        assert SemiMajorAxis(ellipse * -1) == 3
+        assert SemiMinorAxis(ellipse) == 2
+        assert SemiMinorAxis(ellipse * -1) == 2
