4cbfffcb352d6d6bf256f92818ce21852e1f992f
Péter Pallos <pallosp@gmail.com>
2025-04-05 10:47:28 +0200

    Simplified formula for the director circle radius

--- director_circle.py
+++ director_circle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from sympy import Matrix, pprint, simplify, sqrt, symbols
+from sympy import expand, factor, Matrix, pprint, simplify, sqrt, symbols
 
 from lib.circle import Circle
 from lib.central_conic import ConicCenter, SemiMajorAxis, SemiMinorAxis
@@ -8,7 +8,15 @@ from lib.central_conic import ConicCenter, SemiMajorAxis, SemiMinorAxis
 
 a, b, c, d, e, f = symbols("a,b,c,d,e,f")
 conic = Matrix([[a, b, d], [b, c, e], [d, e, f]])
+
 center_x, center_y = ConicCenter(conic)
-radius = sqrt(SemiMajorAxis(conic) ** 2 + SemiMinorAxis(conic) ** 2)
-director_circle = Circle(center_x, center_y, radius)
-pprint(simplify(director_circle))
+radius_square = SemiMajorAxis(conic) ** 2 + SemiMinorAxis(conic) ** 2
+radius_square = factor(expand(simplify(radius_square)))
+radius = sqrt(radius_square)
+
+director_circle = simplify(Circle(center_x, center_y, radius))
+
+print("\nDirector circle radius:")
+pprint(radius)
+print("\nDirector circle matrix:")
+pprint(director_circle)
