0ea54d11ba230e2d5e8283faa5e67bf1ad005fc8
Péter Pallos <pallosp@gmail.com>
2025-04-13 15:02:23 +0200

    LinePair constructor

---
+++ lib/degenerate_conic.py
@@ -0,0 +1,7 @@
+from sympy import Matrix
+
+
+def LinePair(line1: Matrix, line2: Matrix) -> Matrix:
+    assert line1.shape == (3, 1)
+    assert line2.shape == (3, 1)
+    return (line1 * line2.T + line2 * line1.T) / 2
