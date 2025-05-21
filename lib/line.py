c770a978b2f0cc1cbf02996df3a9f2745530f418
Péter Pallos <pallosp@gmail.com>
2025-05-21 12:41:51 +0200

    Functions related to perpendicular lines

--- lib/line.py
+++ lib/line.py
@@ -21,6 +21,18 @@ def ParallelLine(withLine, throughPoint):
     return Matrix([a, b, -a * x - b * y])
 
 
+def PerpendicularLine(toLine, throughPoint):
+    x, y = PointToXY(throughPoint)
+    a, b, _ = toLine
+    return Matrix([-b, a, -b * x + a * y])
+
+
+def PerpendicularBisector(point1, point2):
+    x1, y1 = PointToXY(point1)
+    x2, y2 = PointToXY(point2)
+    return Matrix([x1 - x2, y1 - y2, (x2**2 - x1**2 + y2**2 - y1**2) / 2])
+
+
 def AreParallel(line1, line2):
     """Tells whether line1 and line2 are parallel.
 
@@ -29,7 +41,18 @@ def AreParallel(line1, line2):
     """
     a1, b1, _ = line1
     a2, b2, _ = line2
-    return (a1 * b2 - a2 * b1).is_zero
+    return (a1 * b2 - a2 * b1).expand().is_zero
+
+
+def ArePerpendicular(line1, line2):
+    """Tells whether line1 and line2 are perpendicular.
+
+    Returns True if they are perpendicular, False if not, None if undecidable.
+    Considers the ideal line perpendicular to everything.
+    """
+    a1, b1, _ = line1
+    a2, b2, _ = line2
+    return (a1 * a2 + b1 * b2).expand().is_zero
 
 
 IDEAL_LINE = Matrix([0, 0, 1])
--- tests/line_test.py
+++ tests/line_test.py
@@ -1,7 +1,35 @@
-from sympy import Matrix
-from sympy.abc import x
+from sympy import Matrix, symbols
+from sympy.abc import x, y
 
-from lib.line import AreParallel, HorizontalLine, IDEAL_LINE, ParallelLine, Y_AXIS
+from lib.line import (
+    AreParallel,
+    ArePerpendicular,
+    HorizontalLine,
+    IDEAL_LINE,
+    LineBetween,
+    ParallelLine,
+    PerpendicularBisector,
+    PerpendicularLine,
+    VerticalLine,
+    X_AXIS,
+    Y_AXIS,
+)
+
+
+class TestPerpendicularLine:
+    def test_numeric(self):
+        assert Y_AXIS == PerpendicularLine(X_AXIS, (0, 1))
+
+
+class TestPerpendicularBisector:
+    def test_symbolic(self):
+        x1, y1, x2, y2 = symbols("x1,y1,x2,y2")
+        point1 = (x1, y1)
+        point2 = (x2, y2)
+        bisector = PerpendicularBisector(point1, point2)
+        center = Matrix([(x1 + x2) / 2, (y1 + y2) / 2, 1])
+        assert bisector.dot(center).expand().is_zero
+        assert ArePerpendicular(bisector, LineBetween(point1, point2))
 
 
 class TestAreParallel:
@@ -14,3 +42,13 @@ class TestAreParallel:
     def test_symbolic(self):
         assert AreParallel(Matrix([x, x * 2, 0]), Matrix([x * 2, x * 4, 0]))
         assert AreParallel(Matrix([x, x + x, 0]), Matrix([x, x + 1, 0])) is None
+
+
+class TestArePerpendicular:
+    def test_numeric(self):
+        assert ArePerpendicular(HorizontalLine(1), Y_AXIS)
+        assert ArePerpendicular(HorizontalLine(1), HorizontalLine(2)) is False
+        assert ArePerpendicular(HorizontalLine(1), IDEAL_LINE)
+
+    def test_symbolic(self):
+        assert ArePerpendicular(HorizontalLine(y), VerticalLine(x))
