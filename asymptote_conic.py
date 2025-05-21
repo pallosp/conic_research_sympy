7a7b45d363d224364f72f9a94d30e7a6e4d54af9
Péter Pallos <pallosp@gmail.com>
2025-05-21 15:07:42 +0200

    Asymptote conic

---
+++ asymptote_conic.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from sympy import pprint, symbols
+
+from lib.central_conic import ConicCenter
+from lib.conic import IdealPoints
+from lib.degenerate_conic import LinePair
+from lib.line import LineBetween
+from lib.matrix import ConicMatrix
+
+conic = ConicMatrix(*symbols("a,b,c,d,e,f", positive=True))
+ideal_point_1, ideal_point_2 = IdealPoints(conic)
+center = ConicCenter(conic)
+asymptote1 = LineBetween(center, ideal_point_1)
+asymptote2 = LineBetween(center, ideal_point_2)
+asymptote_conic = LinePair(asymptote1, asymptote2)
+
+for i in range(len(asymptote_conic)):
+    asymptote_conic[i] = asymptote_conic[i].simplify()
+
+print("\nDegenerate conic from the two asymptotes:\n")
+pprint(asymptote_conic)
