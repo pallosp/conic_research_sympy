9275cde14dfa5414910f79554dd30e272d17ca77
Péter Pallos <pallosp@gmail.com>
2025-05-30 15:32:56 +0200

    Function to compute the conic section with the given center and perimeter points.

--- lib/central_conic.py
+++ lib/central_conic.py
@@ -1,6 +1,43 @@
 from sympy import Matrix, Piecewise, sqrt
 
-from lib.matrix import MaxEigenvalue, MinEigenvalue
+from lib.matrix import ConicMatrix, MaxEigenvalue, MinEigenvalue
+from lib.point import PointToXY
+
+
+def ConicFromCenterAndPoints(center, p1, p2, p3) -> Matrix:
+    """Computes the conic section with the given center and perimeter points.
+
+    May return
+     - an ellipse;
+     - a hyperbola;
+     - a parallel line pair;
+     - zero matrix if the solution is ambiguous, which happens when some of the
+       (center, pᵢ, pⱼ) triples are collinear.
+    """
+    x, y = PointToXY(center)
+    x1, y1 = PointToXY(p1)
+    x2, y2 = PointToXY(p2)
+    x3, y3 = PointToXY(p3)
+    x1, x2, x3 = x1 - x, x2 - x, x3 - x
+    y1, y2, y3 = y1 - y, y2 - y, y3 - y
+    m = Matrix(
+        [
+            [x1 * x1, x1 * y1, y1 * y1],
+            [x2 * x2, x2 * y2, y2 * y2],
+            [x3 * x3, x3 * y3, y3 * y3],
+        ]
+    )
+    m1, m2, m3 = m.copy(), m.copy(), m.copy()
+    m1[:, 0] = Matrix([1, 1, 1])
+    m2[:, 1] = Matrix([1, 1, 1])
+    m3[:, 2] = Matrix([1, 1, 1])
+    a = m1.det()
+    b = m2.det() / 2
+    c = m3.det()
+    d = -a * x - b * y
+    e = -b * x - c * y
+    f = -d * x - e * y - m.det()
+    return ConicMatrix(a, b, c, d, e, f)
 
 
 def ConicCenter(conic: Matrix):
--- tests/central_conic_test.py
+++ tests/central_conic_test.py
@@ -1,8 +1,59 @@
-from sympy import symbols
+from sympy import Matrix, symbols
+from sympy.abc import x, y
 
-from lib.central_conic import ConicCenter, SemiMajorAxis, SemiMinorAxis
+from lib.central_conic import (
+    ConicCenter,
+    ConicFromCenterAndPoints,
+    SemiMajorAxis,
+    SemiMinorAxis,
+)
 from lib.circle import Circle, UNIT_CIRCLE
+from lib.conic import ConicFromPoly
+from lib.degenerate_conic import LinePair
+from lib.line import X_AXIS, HorizontalLine
+from lib.matrix import IsScalarMultiple
+from lib.point import ORIGIN
 from lib.transform import ScaleXY, TransformConic
+
+
+class TestConicFromCenterAndPoints:
+    def test_ambiguous_solution(self):
+        center = (4, 2)
+        p1 = (3, 2)
+        p2 = (5, 2)
+        p3 = (4, 3)
+        conic = ConicFromCenterAndPoints(center, p1, p2, p3)
+        assert conic == Matrix.zeros(3, 3)
+
+    def test_circle_centered_at_origin(self):
+        p1 = (3, 4)
+        p2 = (4, 3)
+        p3 = (5, 0)
+        conic = ConicFromCenterAndPoints(ORIGIN, p1, p2, p3)
+        assert IsScalarMultiple(conic, Circle(ORIGIN, 5))
+
+    def test_translated_circle(self):
+        center = (1, 3)
+        p1 = (4, 7)
+        p2 = (5, 6)
+        p3 = (6, 3)
+        conic = ConicFromCenterAndPoints(center, p1, p2, p3)
+        assert IsScalarMultiple(conic, Circle(center, 5))
+
+    def test_hyperbola_centered_at_origin(self):
+        p1 = (1, 6)
+        p2 = (2, 3)
+        p3 = (3, 2)
+        conic = ConicFromCenterAndPoints(ORIGIN, p1, p2, p3)
+        assert IsScalarMultiple(conic, ConicFromPoly(x * y - 6))
+
+    def test_parallel_lines(self):
+        center = (1, 2)
+        p1 = (2, 4)
+        p2 = (3, 4)
+        p3 = (4, 0)
+        conic = ConicFromCenterAndPoints(center, p1, p2, p3)
+        assert IsScalarMultiple(conic, LinePair(X_AXIS, HorizontalLine(4)))
 
 
 class TestConicCenter:
