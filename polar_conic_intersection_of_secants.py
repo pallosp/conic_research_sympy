0000628154719447f731afc1a4550b2e770d8d5b
Péter Pallos <pallosp@gmail.com>
2025-06-25 16:04:27 +0200

    Intersection of secants in polar conics

---
+++ polar_conic_intersection_of_secants.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+from sympy import Matrix, gcd, pi, pprint, symbols
+
+from lib.polar_conic import PointAtAngle
+
+
+a, b, c, d, e, f, g, h, i, alpha, beta = symbols("a,b,c,d,e,f,g,h,i,alpha,beta")
+polar_matrix = Matrix([[a, b, c], [d, e, f], [g, h, i]])
+
+
+def GetIntersectionOfSecants(end_point_func):
+    alpha, beta = symbols("alpha,beta")
+
+    p1 = PointAtAngle(polar_matrix, alpha)
+    p2 = PointAtAngle(polar_matrix, end_point_func(alpha))
+    p3 = PointAtAngle(polar_matrix, beta)
+    p4 = PointAtAngle(polar_matrix, end_point_func(beta))
+
+    secant1 = p1.cross(p2)
+    secant2 = p3.cross(p4)
+
+    common_point = secant1.cross(secant2)
+    common_point /= gcd(common_point[0], common_point[1])
+    for i in range(3):
+        common_point[i] = common_point[i].factor()
+
+    # For certain start point -> end point mapping functions (see below)
+    # alpha and beta vanish.
+    return common_point
+
+
+print("\nPolar conic matrix:\n")
+pprint(polar_matrix)
+
+print("\nIntersections of secants between θ and π-θ:\n")
+pprint(GetIntersectionOfSecants(lambda angle: pi - angle))
+
+print("\nIntersections of secants between θ and -θ:\n")
+pprint(GetIntersectionOfSecants(lambda angle: -angle) * -1)
+
+print("\nIntersections of secants between θ and θ+π:\n")
+pprint(GetIntersectionOfSecants(lambda angle: angle + pi))
