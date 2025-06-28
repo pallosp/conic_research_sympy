96ff7a0f3b53451d54afefc8039605ed8a5c344a
Péter Pallos <pallosp@gmail.com>
2025-06-28 08:45:57 +0200

    Improve output readability

--- focus_directrix_eccentricity.py
+++ focus_directrix_eccentricity.py
@@ -19,8 +19,12 @@ from sympy import (
 from lib.conic import ConicFromFocusAndDirectrix
 
 
-def pprint_indented(expr):
+def print_indented(expr):
     print(indent(pretty(expr), "  "))
+
+
+def println_indented(expr):
+    print(indent(pretty(expr), "  ") + "\n")
 
 
 fx, fy, a, b, c, ecc, L = symbols("x,y,a,b,c,e,lambda", real=True)
@@ -42,19 +46,19 @@ coeff_eq = [
     Eq(F, conic[8]),
 ]
 for eq in coeff_eq:
-    pprint_indented(eq)
+    print_indented(eq)
 
 print("\nNormalize the directrix so that a² + b² = 1:\n")
 
 for i in range(len(coeff_eq)):
     coeff_eq[i] = coeff_eq[i].subs(a * a + b * b, 1)
-    pprint_indented(coeff_eq[i])
+    print_indented(coeff_eq[i])
 
 print("\nDeterminant of the conic matrix:\n")
 
 det = symbols("det")
 det_eq = Eq(det, conic.det().factor().subs(a * a + b * b, 1))
-pprint_indented(det_eq)
+print_indented(det_eq)
 
 print("\nThis implies that sign(λ) = sign(det).")
 print("Note that the formula breaks down for circles.\n")
@@ -111,42 +115,43 @@ eigenvalue_eqs = [
 ]
 
 for eq in eigenvalue_eqs:
-    pprint_indented(eq)
+    println_indented(eq)
 
-print("\nAs sign(λ) = sign(det):\n")
+print("As sign(λ) = sign(det):\n")
 
 eigenvalue_diff_eq = Eq((max_eigen - min_eigen) * sign(det, evaluate=False), ecc**2 * L)
-pprint_indented(eigenvalue_diff_eq)
-print()
-lambda_eq = factor(Div(Sub(eigenvalue_diff_eq, eigenvalue_sum_eq), 2))
-pprint_indented(lambda_eq)
-lambda_eq = Eq(L, Piecewise((-min_eigen, det > 0), (-max_eigen, True)))
-pprint_indented(lambda_eq)
-ecc_square_eq = Swap(Div(eigenvalue_diff_eq, L))
-print()
-pprint_indented(ecc_square_eq)
+println_indented(eigenvalue_diff_eq)
 
-print("\nDirectrix normal vector a.k.a. focal axis direction:\n")
+lambda_eq = Swap(factor(Div(Sub(eigenvalue_diff_eq, eigenvalue_sum_eq), 2)))
+println_indented(lambda_eq)
+
+lambda_eq_piecewise = Eq(L, Piecewise((-min_eigen, det > 0), (-max_eigen, True)))
+println_indented(lambda_eq_piecewise)
+
+ecc_square_eq = Swap(Div(eigenvalue_diff_eq, L))
+println_indented(ecc_square_eq)
+
+print("Directrix normal vector a.k.a. focal axis direction:\n")
 
 aa_eq = Eq(a**2, solve(coeff_eq[0], a**2)[0])
 bb_eq = Eq(b**2, solve(coeff_eq[2], b**2)[0])
 ab_eq = Swap(Div(coeff_eq[1], ecc**2 * L))
-pprint_indented(aa_eq)
-pprint_indented(bb_eq)
-pprint_indented(ab_eq)
+println_indented(aa_eq)
+println_indented(bb_eq)
+println_indented(ab_eq)
 
-print("\nAs a² + b² = 1, substitute a = cos(θ), b = sin(θ)\n")
+print("As a² + b² = 1, substitute a = cos(θ), b = sin(θ)\n")
 
 theta = symbols("theta")
-pprint_indented(Sub(aa_eq, bb_eq))
-pprint_indented(Sub(aa_eq, bb_eq).subs({a: cos(theta), b: sin(theta)}))
+println_indented(Sub(aa_eq, bb_eq))
+println_indented(Sub(aa_eq, bb_eq).subs({a: cos(theta), b: sin(theta)}))
 cos2_eq = Sub(aa_eq, bb_eq).subs({a: cos(theta), b: sin(theta)}).simplify()
-pprint_indented(cos2_eq)
-pprint_indented(Mul(ab_eq, 2))
-pprint_indented(Mul(ab_eq, 2).subs({a: cos(theta), b: sin(theta)}))
+println_indented(cos2_eq)
+println_indented(Mul(ab_eq, 2))
+println_indented(Mul(ab_eq, 2).subs({a: cos(theta), b: sin(theta)}))
 sin2_eq = Mul(ab_eq, 2).subs({a: cos(theta), b: sin(theta)}).simplify()
-pprint_indented(sin2_eq)
+println_indented(sin2_eq)
 theta_eq = Eq(theta, atan2(sin2_eq.rhs, cos2_eq.rhs) / 2)
-pprint_indented(theta_eq)
-pprint_indented(theta_eq.subs(ecc**2 * L, sign(ecc**2 * L)))
-pprint_indented(theta_eq.subs(ecc**2 * L, 1 / sign(det)))
+println_indented(theta_eq)
+println_indented(theta_eq.subs(ecc**2 * L, sign(ecc**2 * L)))
+println_indented(theta_eq.subs(ecc**2 * L, 1 / sign(det)))
