34099b1ed91d052459101445669858d2e21a82dd
Péter Pallos <pallosp@gmail.com>
2025-06-30 13:33:13 +0200

    Add CircleRadius(conic) and DirectorCircle(conic)

--- lib/circle.py
+++ lib/circle.py
@@ -1,3 +1,4 @@
+from sympy import Matrix, sqrt
 from lib.matrix import ConicMatrix
 from lib.point import ORIGIN, PointToXY
 
@@ -5,6 +6,28 @@ from lib.point import ORIGIN, PointToXY
 def Circle(center, r):
     x, y = PointToXY(center)
     return ConicMatrix(-1, 0, -1, x, y, r * r - x * x - y * y)
+
+
+def CircleRadius(circle):
+    """Computes the radius of a circle conic.
+
+    The result is not specified if the conic matrix is not a circle.
+
+    The computation is based on director_circle.py.
+    """
+    a, b, c = circle[0], circle[1], circle[4]
+    return sqrt(-circle.det() * (a + c) / 2) / (a * c - b * b)
+
+
+def DirectorCircle(conic):
+    a, _, _, _, c, _, d, e, f = conic.adjugate()
+    return Matrix(
+        [
+            [-1, 0, d / f],
+            [0, -1, e / f],
+            [d / f, e / f, -(a + c) / f],
+        ]
+    )
 
 
 UNIT_CIRCLE = Circle(ORIGIN, 1)
--- tests/circle_test.py
+++ tests/circle_test.py
@@ -1,7 +1,8 @@
-from sympy import Matrix
+from sympy import Matrix, pi, sqrt
 
+from lib.ellipse import Ellipse
 from lib.matrix import IsScalarMultiple
-from lib.circle import Circle, UNIT_CIRCLE
+from lib.circle import UNIT_CIRCLE, Circle, CircleRadius, DirectorCircle
 
 
 def test_unit_circle():
@@ -11,3 +12,14 @@ def test_unit_circle():
 def test_circle():
     circle = Circle((1, 2), 3)
     assert IsScalarMultiple(circle, Matrix([[-1, 0, 1], [0, -1, 2], [1, 2, 4]]))
+
+
+def test_circle_radius():
+    assert CircleRadius(Circle((1, 2), 3)) == 3
+    assert CircleRadius(UNIT_CIRCLE * -2) == 1
+
+
+def test_director_circle():
+    assert DirectorCircle(Circle((1, 2), 3)) == Circle((1, 2), 3 * sqrt(2))
+    assert DirectorCircle(Ellipse((1, 2), 3, 4)) == Circle((1, 2), 5)
+    assert DirectorCircle(Ellipse((1, 2), 3, 4, r1_angle=pi / 4)) == Circle((1, 2), 5)
