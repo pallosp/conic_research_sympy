b27f633c262b466e40f59943ed027a30c58fdc8d
Péter Pallos <pallosp@gmail.com>
2025-09-26 12:56:52 +0200

    Use printf for better portability

--- src/research/run-all.sh
+++ src/research/run-all.sh
@@ -6,10 +6,10 @@ cd "$(dirname "$0")"
 for script in *.py; do
   [ "$script" = "__init__.py" ] && continue
   echo
-  echo -e "\033[1mRunning $script...\033[0m"
+  printf "\033[1mRunning $script...\033[0m"
   echo
   ./"$script"
 done
 
 echo
-echo "\033[1mAll scripts ran successfully.\033[0m"
+printf "\033[1mAll scripts ran successfully.\033[0m"
