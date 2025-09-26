1c9d2f4f1f95b097302bc395c989e2277f6285fb
Péter Pallos <pallosp@gmail.com>
2025-09-26 14:33:11 +0200

    Simplify polar_conic_center.py

--- src/research/polar_conic_center.py
+++ src/research/polar_conic_center.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from sympy import MatMul, Matrix, gcd, pprint, symbols
+from sympy import MatMul, Matrix, factor, gcd, pprint, symbols
 
 from lib.polar_conic import ConicFromPolarMatrix
 
@@ -10,10 +10,9 @@ polar_matrix = Matrix([[a, b, c], [d, e, f], [g, h, i]])
 conic = ConicFromPolarMatrix(polar_matrix)
 center = conic.row(0).cross(conic.row(1)).T
 center /= -gcd(center[0], center[1])
-for index in range(3):
-    center[index] = center[index].factor()
+center = center.applyfunc(factor)
 
 assert center == polar_matrix * Matrix([g, h, -i])
 
-pprint("\nCenter point of a polar conic:\n\n")
+print("\nCenter point of a polar conic:\n")
 pprint(MatMul(polar_matrix, Matrix([g, h, -i])))
