c775743a5f89014e25ed6146e0af3b196c9ad99f
Péter Pallos <pallosp@gmail.com>
2025-10-07 15:34:41 +0200

    Cosmetic improvements in scale_conic_from_center

--- src/research/scale_conic_from_center.py
+++ src/research/scale_conic_from_center.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 
-from sympy import Matrix, Symbol, gcd, pprint, simplify, symbols
+from sympy import Matrix, Symbol, gcd, simplify, symbols
 
 from lib.central_conic import ConicCenter
 from lib.matrix import ConicMatrix
 from lib.transform import Scale, TransformConic
+from research.util import println_indented
 
 print("\nScaling a conic from its center by a factor of λ\n")
 
@@ -12,7 +13,7 @@ print("Original conic:\n")
 
 a, b, c, d, e, f = symbols("a,b,c,d,e,f")
 conic = ConicMatrix(a, b, c, d, e, f)
-pprint(conic)
+println_indented(conic)
 
 print("\nScaled conic - only the f coefficient differs:\n")
 
@@ -23,4 +24,4 @@ scaled_conic = Matrix(scaled_conic / gcd(list(scaled_conic)))
 scaled_conic[8] = (scaled_conic[8] - f).factor() + f
 scaled_conic[8] = scaled_conic[8].subs(conic.det(), Symbol("det"))
 
-pprint(scaled_conic)
+println_indented(scaled_conic)
