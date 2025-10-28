dbc20b34d8342f2cb542644e1683d6a24eb060ab
Péter Pallos <pallosp@gmail.com>
2025-10-28 10:03:11 +0100

    Relationship between linear eccentricity and the radii

---
+++ src/research/linear_eccentricity_radii_relationship.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+from sympy import Abs, Eq, pprint, symbols
+
+from lib.central_conic import linear_eccentricity, primary_radius, secondary_radius
+from lib.conic_classification import ConicNormFactor
+from lib.matrix import conic_matrix
+
+conic = conic_matrix(*symbols("a b c d e f", real=True))
+det = symbols("det", real=True)
+
+le_square = (linear_eccentricity(conic) ** 2).subs(conic.det(), det).factor()
+
+pprint(Eq(symbols("linear_eccentricity") ** 2, le_square))
+print()
+
+r1 = primary_radius(conic)
+r2 = secondary_radius(conic)
+
+radius_square_diff = (
+    (r1**2 - r2**2)
+    .simplify()
+    .factor()
+    .subs(conic.det(), det)
+    .subs(ConicNormFactor(conic) * det, Abs(det))
+)
+
+pprint(Eq(symbols("r1") ** 2 - symbols("r2") ** 2, radius_square_diff))
+print()
