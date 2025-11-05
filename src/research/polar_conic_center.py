67f339f67afeb7b28a257a89e659819fa4825a55
Péter Pallos <pallosp@gmail.com>
2025-11-05 17:07:21 +0100

    Refactor research/polar_conic_center

--- src/research/polar_conic_center.py
+++ src/research/polar_conic_center.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python3
 
-from sympy import MatMul, Matrix, factor, gcd, pprint, symbols
+from sympy import MatMul, Matrix, factor, gcd, symbols
 
+from lib.conic import projective_conic_center
 from lib.polar_conic import conic_from_polar_matrix
+from research.util import println_indented
 
-a, b, c, d, e, f, g, h, i = symbols("a,b,c,d,e,f,g,h,i")
-polar_matrix = Matrix([[a, b, c], [d, e, f], [g, h, i]])
+polar_matrix = Matrix(3, 3, symbols("a,b,c,d,e,f,g,h,i"))
 
 conic = conic_from_polar_matrix(polar_matrix)
-center = conic.row(0).cross(conic.row(1)).T
+center = projective_conic_center(conic)
 center /= -gcd(center[0], center[1])
 center = center.applyfunc(factor)
 
-assert center == polar_matrix * Matrix([g, h, -i])
+print("\nCenter point of a conic in polar representation:\n")
 
-print("\nCenter point of a polar conic:\n")
-pprint(MatMul(polar_matrix, Matrix([g, h, -i])))
+println_indented(MatMul(polar_matrix, (polar_matrix.inv() * center).applyfunc(factor)))
