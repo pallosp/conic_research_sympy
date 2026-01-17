34a8649a8923f8c6138c5d6251d509634a799d15
Péter Pallos <pallosp@gmail.com>
2026-01-17 13:22:54 +0100

    Compute sin(axis_asymptote_angle) and tan(axis_asymptote_angle)

--- src/research/conic_properties/asymptote_angle.py
+++ src/research/conic_properties/asymptote_angle.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 
-from sympy import Eq, Limit, cos, oo, solve, symbols
+from sympy import Abs, Eq, I, Limit, cos, oo, sign, sin, solve, sqrt, symbols, tan
 
+from lib.central_conic import primary_radius, secondary_radius
+from lib.conic_direction import ConicNormFactor
+from lib.matrix import conic_matrix
 from research.sympy_utils import eq_chain, println_indented
 
 print("\nAngle between the focal axis and an asymptote of a hyperbola\n")
@@ -27,7 +30,9 @@ println_indented(law_of_cosines)
 solution = solve(law_of_cosines, cos_f1)[0]
 println_indented(Eq(cos_f1, solution))
 
-print("The angle between the focal axis and an asymptote:\n")
+print("Angle between the focal axis and an asymptote:\n")
+
+# cos
 
 cos_f1_limit = Limit(cos_f1, x, oo)
 aa_angle = symbols("axis_asymptote_angle")
@@ -39,4 +44,33 @@ println_indented(
         -cos_f1_limit.subs(cos_f1, solution).doit(),
         1 / symbols("eccentricity"),
     ),
+)
+
+# sin
+
+sec_radius = symbols("r_2")
+println_indented(
+    eq_chain(
+        sin(aa_angle),
+        sqrt(lin_ecc**2 - radius**2) / lin_ecc,
+        sqrt(-(sec_radius**2)) / lin_ecc,
+        sec_radius * I / lin_ecc,
+    )
+)
+
+# tan
+
+a, b, c, d, e, f = symbols("a b c d e f", real=True)
+conic = conic_matrix(a, b, c, d, e, f)
+det = symbols("det", nonzero=True)
+tan_aa = I * secondary_radius(conic) / primary_radius(conic)
+tan_aa = sqrt(tan_aa**2).simplify().subs(ConicNormFactor(conic), sign(det))
+num, denom = (tan_aa**2).as_numer_denom()
+println_indented(
+    eq_chain(
+        tan(aa_angle),
+        I * sec_radius / radius,
+        tan_aa,
+        Abs(num) / sqrt(num * denom).expand().factor(),
+    )
 )
